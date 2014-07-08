649121aec5c4696fb263be68aba35d7ad1a1188f
Naranjo Manuel Francisco <naranjo.manuel@gmail.com>
2014-07-08 14:26:48 -0300

    settings cleanup

--- project/settings/dev.py
+++ project/settings/dev.py
@@ -6,11 +6,3 @@ try:
 	from .local import *
 except ImportError:
 	pass
-
-
-SMSGATE_CREDENTIALS = {
-    'api_id': 99,
-    'login': 'vasya',
-    'password': 'pupkin',
-}
-SMSGATE_HTTP_TIMEOUT = 5
