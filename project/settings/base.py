6270aa1288c67cbeeeff78356075842a4655e53e
Naranjo Manuel Francisco <naranjo.manuel@gmail.com>
2014-07-08 14:25:35 -0300

    fixing paths!

--- project/settings/base.py
+++ project/settings/base.py
@@ -7,7 +7,7 @@ PROJECT_ROOT = os.path.join(os.path.dirname(__file__), '..', '..')
 
 # The directory with this project's templates, settings, urls, static dir,
 # wsgi.py, fixtures, etc.
-PROJECT_PATH = os.path.join(PROJECT_ROOT, 'project')
+PROJECT_PATH = os.path.join(PROJECT_ROOT, 'core')
 
 # Modify sys.path to include the lib directory
 sys.path.append(os.path.join(PROJECT_ROOT, "lib"))
@@ -65,7 +65,7 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'public', 'media')
+MEDIA_ROOT = os.path.join(PROJECT_PATH, 'media')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -76,7 +76,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_ROOT = os.path.join(PROJECT_PATH, 'static')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -87,7 +87,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(PROJECT_PATH, 'static'),
+    #os.path.join(PROJECT_PATH, 'static'),
 )
 
 # List of finder classes that know how to find static files in
