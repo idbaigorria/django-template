e4884935b0895a3de5b4aaaaafb7cdee3a20bbea
Naranjo Manuel Francisco <naranjo.manuel@gmail.com>
2014-07-08 14:27:02 -0300

    Code formatting

--- project/settings/base.py
+++ project/settings/base.py
@@ -127,7 +127,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
+    #'debug_toolbar.middleware.DebugToolbarMiddleware',
 )
 
 ROOT_URLCONF = 'project.urls'
