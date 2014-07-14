062e00baa83ff62db880f0228b6c177c4189a6b6
Naranjo Manuel Francisco <naranjo.manuel@gmail.com>
2014-07-14 13:58:02 -0300

    Updating provision, adding kannel setup

--- etc/install/install.sh
+++ etc/install/install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -ex
+
 # Script to set up a Django project on Vagrant.
 
 # Installation settings
@@ -23,20 +25,24 @@ export LANGUAGE=en_GB.UTF-8
 export LANG=en_GB.UTF-8
 export LC_ALL=en_GB.UTF-8
 
+EZ_SETUP=https://bitbucket.org/pypa/setuptools/raw/bootstrap/ez_setup.py
+
 # Install essential packages from Apt
 apt-get update -y
 # Python dev packages
 apt-get install -y build-essential python python-dev
 # python-setuptools being installed manually
-wget https://bitbucket.org/pypa/setuptools/raw/bootstrap/ez_setup.py -O - | python
-# Dependencies for image processing with Pillow (drop-in replacement for PIL)
+wget $EZ_SETUP -O - | python
+# Dependencies for image processing with Pillow (replacement for PIL)
 # supporting: jpeg, tiff, png, freetype, littlecms
-# (pip install pillow to get pillow itself, it is not in requirements.txt)
-apt-get install -y libjpeg-dev libtiff-dev zlib1g-dev libfreetype6-dev liblcms2-dev
-# Git (we'd rather avoid people keeping credentials for git commits in the repo, but sometimes we need it for pip requirements that aren't in PyPI)
+# (pip install pillow to get pillow itself, not in requirements.txt)
+apt-get install -y libjpeg-dev libtiff-dev zlib1g-dev libfreetype6-dev \
+    liblcms2-dev
+# Git (we'd rather avoid people keeping credentials for git commits
+# in the repo, but sometimes we need it for pip requirements that
+# aren't in PyPI)
 apt-get install -y git
-# Kannel
-apt-get install -y kannel
+
 # mcedit
 apt-get install -y mc
 # ppa stuff
@@ -56,8 +62,9 @@ chmod a+rw /var/log/django
 if ! command -v psql; then
 
     apt-get install -y postgresql-$PGSQL_VERSION libpq-dev
-    apt-get install -y postgresql-$PGSQL_VERSION-postgis postgis
-    cp $PROJECT_DIR/etc/install/pg_hba.conf /etc/postgresql/$PGSQL_VERSION/main/
+    apt-get install -y postgresql-$PGSQL_VERSION-postgis-2.0 postgis
+    cp $PROJECT_DIR/etc/install/pg_hba.conf \
+        /etc/postgresql/$PGSQL_VERSION/main/
     /etc/init.d/postgresql reload
 fi
 
@@ -69,25 +76,31 @@ if [[ ! -f /usr/local/bin/virtualenv ]]; then
     pip install virtualenv virtualenvwrapper stevedore virtualenv-clone
 fi
 
+if [[ ! -f /etc/init.d/kannel ]]; then
+    # Kannel
+    apt-get install -y kannel
+    cp $PROJECT_DIR/etc/install/kannel.conf /etc/kannel/kannel.conf
+fi
+
 # bash environment global setup
 cp -p $PROJECT_DIR/etc/install/bashrc /home/vagrant/.bashrc
 su - vagrant -c "mkdir -p /home/vagrant/.pip_download_cache"
 
-# Node.js, CoffeeScript and LESS
-if ! command -v npm; then
-    wget http://nodejs.org/dist/v0.10.0/node-v0.10.0.tar.gz
-    tar xzf node-v0.10.0.tar.gz
-    cd node-v0.10.0/
-    ./configure && make && make install
-    cd ..
-    rm -rf node-v0.10.0/ node-v0.10.0.tar.gz
-fi
-if ! command -v coffee; then
-    npm install -g coffee-script
-fi
-if ! command -v lessc; then
-    npm install -g less
-fi
+# # Node.js, CoffeeScript and LESS
+# if ! command -v npm; then
+#     wget http://nodejs.org/dist/v0.10.0/node-v0.10.0.tar.gz
+#     tar xzf node-v0.10.0.tar.gz
+#     cd node-v0.10.0/
+#     ./configure && make && make install
+#     cd ..
+#     rm -rf node-v0.10.0/ node-v0.10.0.tar.gz
+# fi
+# if ! command -v coffee; then
+#     npm install -g coffee-script
+# fi
+# if ! command -v lessc; then
+#     npm install -g less
+# fi
 
 # ---
 
@@ -101,12 +114,16 @@ EOF
 # virtualenv setup for project
 su - vagrant -c "/usr/local/bin/virtualenv $VIRTUALENV_DIR && \
     echo $PROJECT_DIR > $VIRTUALENV_DIR/.project && \
-    PIP_DOWNLOAD_CACHE=/home/vagrant/.pip_download_cache $VIRTUALENV_DIR/bin/pip install -r $PROJECT_DIR/requirements.txt"
+    PIP_DOWNLOAD_CACHE=/home/vagrant/.pip_download_cache \
+    $VIRTUALENV_DIR/bin/pip install -r $PROJECT_DIR/requirements.txt"
 
 echo "workon $VIRTUALENV_NAME" >> /home/vagrant/.bashrc
 
-# Set execute permissions on manage.py, as they get lost if we build from a zip file
+# Set execute permissions on manage.py, as they get lost if we build \
+# from a zip file
 chmod a+x $PROJECT_DIR/manage.py
 
 # Django project setup
-su - vagrant -c "source $VIRTUALENV_DIR/bin/activate && cd $PROJECT_DIR && ./manage.py syncdb --noinput && ./manage.py migrate"
+su - vagrant -c "source $VIRTUALENV_DIR/bin/activate && \
+   cd $PROJECT_DIR && ./manage.py syncdb --noinput && \
+   ./manage.py migrate"
