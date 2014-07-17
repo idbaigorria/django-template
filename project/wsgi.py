44aaa6cc59dfb59dde343a348825fa9279182dec
Naranjo Manuel Francisco <naranjo.manuel@gmail.com>
2014-07-17 11:28:13 -0300

    Enable automatic reload with uwsgi

--- project/wsgi.py
+++ project/wsgi.py
@@ -27,3 +27,15 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE",
 
 import django.core.handlers.wsgi
 application = django.core.handlers.wsgi.WSGIHandler()
+
+from django.conf import settings
+
+if settings.DEBUG:
+    import uwsgi
+    from uwsgidecorators import timer
+    from django.utils import autoreload
+
+    @timer(3)
+    def change_code_gracefull_reload(sig):
+        if autoreload.code_changed():
+            uwsgi.reload()
