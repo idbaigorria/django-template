bd65c2d9cfee3c401b058fc1ac49338781d50c74
Naranjo Manuel Francisco <naranjo.manuel@gmail.com>
2014-07-17 08:52:45 -0300

    Enhance urls readability

--- project/urls.py
+++ project/urls.py
@@ -11,30 +11,62 @@ from rapidsms.contrib.httptester import urls
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^admin/', include(admin.site.urls)),
-    # RapidSMS core URLs
-    (r'^accounts/', include('rapidsms.urls.login_logout')),
-    #url(r'^$', 'rapidsms.views.dashboard', name='rapidsms-dashboard'),
-    # RapidSMS contrib app URLs
-    (r'^httptester/', include('rapidsms.contrib.httptester.urls')),
-    #(r'^locations/', include('rapidsms.contrib.locations.urls')),
-    (r'^messagelog/', include('rapidsms.contrib.messagelog.urls')),
-    (r'^messaging/', include('rapidsms.contrib.messaging.urls')),
-    (r'^registration/', include('rapidsms.contrib.registration.urls')),
+    url(r'^admin/',
+        include(admin.site.urls),
+        name="admin"),
+
+    # RapidSMS
+    (
+        r'^accounts/',
+        include('rapidsms.urls.login_logout')
+    ),
+    (
+        r'^httptester/',
+        include('rapidsms.contrib.httptester.urls')
+    ),
+    (
+        r'^locations/',
+        include('rapidsms.contrib.locations.urls')
+    ),
+    (
+        r'^messagelog/',
+        include('rapidsms.contrib.messagelog.urls')
+    ),
+    (
+        r'^messaging/',
+        include('rapidsms.contrib.messaging.urls')
+    ),
+    (
+        r'^registration/',
+        include('rapidsms.contrib.registration.urls')
+    ),
 
     # Third party URLs
-    (r'^selectable/', include('selectable.urls')),
+    (
+        r'^selectable/',
+        include('selectable.urls')
+    ),
 
     # kannel backend urls
     url(r"^backend/kannel-usb0-smsc/$",
-        KannelBackendView.as_view(backend_name="kannel-usb0-smsc")),
-    url(r'^kannel/', include('rapidsms.backends.kannel.urls')),
+        KannelBackendView.as_view(
+            backend_name="kannel-usb0-smsc")),
+    url(r'^kannel/',
+        include('rapidsms.backends.kannel.urls')),
 
     # public_lights
-    url('^lights/', include('public_lights.urls', namespace='public_lights')),
+    url('^lights/',
+        include('public_lights.urls',
+                namespace='public_lights')
+    ),
 
     # rest
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^api-auth/',
+        include('rest_framework.urls',
+                namespace='rest_framework')),
+
+) + static(settings.MEDIA_URL,
+           document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     import debug_toolbar
